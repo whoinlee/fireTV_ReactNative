3280274728524e4eba822cf50580a33ea9d58a45
Whoin Lee <whoinlee@gmail.com>
2018-06-12 16:40:56 -0400

    pane up & down udpated with onFocus handlers

--- src/POCContainer.js
+++ src/POCContainer.js
@@ -14,7 +14,7 @@ import {
   TVEventHandler,
   View
 } from 'react-native';
-import KeyEvent from 'react-native-keyevent';
+// import KeyEvent from 'react-native-keyevent';
 
 import config from './config';
 // import keyCodes from './keyCodes';
@@ -25,6 +25,30 @@ import HomeShelvesPane from './components/HomeShelvesPane';
 
 
 //---------- from config -----------------------------------------//
+/*
+  //-- base dimension and density
+  density: PixelRatio.get(),    //TV density
+  stageW: 1920,
+  stageH: 1080,
+
+  //-- animation related
+  stdDuration: 500,             //in milliseconds = .5 sec,
+  shortDuration: 350,           //in milliseconds = .35 sec,
+  waitToLargeBloomDuration: 4000,
+
+  //-- pane initial location
+  initGlobalNavY: 0,
+  initHomeHeroY: 165,
+  initHomeShelvesY: 836,
+
+  //-- pane initial dimension
+  initGlobalNavH: 100,
+  initHomeHeroH: 606,
+
+  //-- opacity related
+  unselectedOpacity: 1,
+  selectedOpacity: .6,
+*/
 const RATIO               = config.density;
 const STD_DURATION        = config.stdDuration;
 const SHORT_DURATION      = config.shortDuration;
@@ -34,103 +58,116 @@ const INIT_HOME_HERO_Y    = config.initHomeHeroY/RATIO;           //-- 165 = (10
 const INIT_HOME_SHELVES_Y = config.initHomeShelvesY/RATIO;        //-- 836 = (100(globalNav)+65(offset)+606(homeHero)+65) = 836 
 //
 const V_MIDDLE_Y          = Math.floor(config.stageH/(2*RATIO));  //-- vertical middle location of the stage
-//----------------------------------------------------------------//
+const UNSELECTED_OPACITY  = config.unselectedOpacity;
+const SELECTED_OPACITY    = config.selectedOpacity;
 
+/*
+//-- homeShelvesPane location & dimension related
+homeShelves: {
+  initShelfY: 62,             //-- distance from the top of homeShelvesPane container to the top of 1st shelf container
+
+  //-- shelf title related
+  baseTitleH: 40,             //-- unselected shelf title height (!!!not same as the font size, 28)
+  focusedTitleH: 60,          //-- update!!!
+  titleToTileOffset: 10,      //-- distance from the bottom of shelf title to the top of shelf tile
+  
+  //-- tile dimensions
+  baseTileW: 320,             //-- unselected base tile width, 320x180
+  baseTileH: 180,             //-- unselected base tile height
+  focusedTileW: 590,          //-- focused tile width, on a selected shelf, 590x332
+  focusedTileH: 332,          //-- focused tile height, on a selected shelf
+  bloomedTileW: 1056,         //-- large bloomed tile width, on a selected shelf, 1056x594
+  bloomedTileH: 594,          //-- large bloomed tile height, on a selected shelf
+  // ??? focused unselected tile dimension ??? (375x210)
+  // ??? bloomed unselected tile dimension ??? (782x440)
+
+  //-- shelf related
+  baseShelfOffsetX: 0,        //-- distance between tiles on an unselected shelf
+  baseShelfOffsetY: 106,      //-- distance between unselected shelves
+  baseShelfH: 336,            //-- baseTitleH (40) + titleToTileOffset (10) + baseTileH (180) + baseShelfOffsetY (106) = 336
+
+  focusedShelfShiftY: 76,     //-- the y location shift of unselected shelves on selected shelf being focused: (focusedTileH (332) - baseTileH (180))/2 = 76
+  bloomedShelfShiftY: 131,    //-- the y location shift of unselected shelves on selected shelf being large bloomed: (bloomedTileH (594) - focusedTileH (332))/2 = 131
+  
+  focusedShelfOffsetX: 25,    //-- distance between tiles on a focused shelf
+  focusedShelfOffsetY: 182,   //-- baseShelfOffsetY (106) + focusedShelfShiftY (76) = 182
+  focusedShelfH: 584,         //-- focusedTitleH (60) + titleToTileOffset (10) + focusedTileH (332) + focusedShelfOffsetY (182) = 584
+
+  bloomedShelfOffsetX: 57,    //-- distance between tiles on a focused shelf ?????
+  bloomedShelfOffsetY: 313,   //-- focusedShelfOffsetY (182) + bloomedShelfShiftY (131) = 313
+  bloomedShelfH: 543          //-- focusedTitleH (60) + titleToTileOffset (10) + bloomedTileH (594) + bloomedShelfOffsetY (313) = 977
+}
+*/
+const INIT_SHELF_Y          = config.homeShelves.initShelfY/RATIO;
+
+const BASE_TITLE_H          = config.homeShelves.baseTitleH/RATIO;         
+const TITLE_TO_TILE_OFFSET  = config.homeShelves.titleToTileOffset/RATIO;
+
+const BASE_TILE_H           = config.homeShelves.baseTileH/RATIO;
+const FOCUSED_TILE_H        = config.homeShelves.focusedTileH/RATIO; 
+
+const BASE_SHELF_OFFSET_Y   = config.homeShelves.baseShelfOffsetY/RATIO;
+const BASE_SHELF_H          = config.homeShelves.baseShelfH/RATIO;
+// const BASE_SHELF_H = BASE_SHELF_OFFSET_Y;
+
+const FOCUSED_SHELF_SHIFT_Y = config.homeShelves.focusedShelfShiftY/RATIO;
+const BLOOMED_SHELF_SHIFT_Y = config.homeShelves.bloomedShelfShiftY/RATIO;
+
+// const FOCUSED_SHELF_OFFSET_Y  = BASE_SHELF_H + FOCUSED_SHELF_SHIFT_Y;
+const FOCUSED_SHELF_OFFSET_Y= config.homeShelves.focusedShelfOffsetY/RATIO;
+const FOCUSED_SHELF_H       = config.homeShelves.focusedShelfH/RATIO;  
+/* ------------------------------------------ */
+
+
+/* ------------------------------------------ */
+/* POCContainer specific constants            */
+/* ------------------------------------------ */
 const FOCUS_LOC_ARR       = ['globalNavPane', 'homeHeroPane', 'homeShelvesPane'];
 const GLOBAL_NAV_INDEX    = 0;
 const HOME_HERO_INDEX     = 1;
 const HOME_SHELVES_INDEX  = 2;
 
-/* ----- from HomeShelvesPane design -------- */
-const INIT_SHELF_Y        = 62/RATIO;         //-- from container top to the 1st shelf title
-//
-//const BASE_TITLE_H        = 28/RATIO;       //-- wrong!!! ====> turned out to be '20'
-const BASE_TITLE_H        = 40/RATIO;         
-const TITLE_N_TILE_OFFSET = 10/RATIO; 
-const BASE_TILE_H         = 180/RATIO;
-const FOCUSED_TILE_H      = 332/RATIO; 
-/* ------------------------------------------ */
-
-const BASE_SHELF_OFFSET   = 106/RATIO;
-const BASE_SHELF_H = BASE_TITLE_H + TITLE_N_TILE_OFFSET + BASE_TILE_H + BASE_SHELF_OFFSET;
-// const BASE_SHELF_H = BASE_SHELF_OFFSET_Y;
-const FOCUSED_SHELF_SHIFT_Y   = Math.floor(76/RATIO);
-const BLOOMED_SHELF_SHIFT_Y   = Math.floor(131/RATIO);
-// const FOCUSED_SHELF_OFFSET_Y  = BASE_SHELF_H + FOCUSED_SHELF_SHIFT_Y;
-const FOCUSED_SHELF_OFFSET_Y  = BASE_SHELF_OFFSET + FOCUSED_SHELF_SHIFT_Y;
-const FOCUSED_SHELF_H         = BASE_SHELF_H + FOCUSED_SHELF_SHIFT_Y;
-
-// console.log("INFO FOCUSED_SHELF_SHIFT 1 ?? " + FOCUSED_SHELF_SHIFT_Y)
-// console.log("INFO FOCUSED_SHELF_H 1 ?? " + FOCUSED_SHELF_H)
-
-// const ControllableGlobalNav = toControllable(GlobalNavPane)
-// const ControllableHomeHero = toControllable(HomeHeroPane)
-// const ControllableHomeShelves = toControllable(HomeShelvesPane)
 
 export default class POCContainer extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      //activeControllable: null,
-      //controllables: [],
       animOpacity0: new Animated.Value(1),    //-- animation instance for 'globalNav' with initial opacity value of '1'
       animOpacity1: new Animated.Value(1),    //-- for 'homeHero'
       animOpacity2: new Animated.Value(1),    //-- for 'homeShelves'
       animLocation0: new Animated.Value(INIT_GLOBAL_NAV_Y),   //-- animation instance for 'globalNav' with initial location value of INIT_GLOBAL_NAV_Y
       animLocation1: new Animated.Value(INIT_HOME_HERO_Y),    //-- for 'homeHero'
       animLocation2: new Animated.Value(INIT_HOME_SHELVES_Y), //-- for 'homeShelves'
-      isGuideVisible: true,
+      isGuideVisible: false,
     }
+
+    this.tvEventHandler = new TVEventHandler()
 
     this.selectablePanes = []
     this.currFocusLocIndex = GLOBAL_NAV_INDEX //CHECK : needs to move to the state
     this.selectedShelfIndex = -1  
     this.currHomeShelvesY = INIT_HOME_SHELVES_Y
     this.isFirstShelfSelected = false
-    
+
+    //-- set panes' initial locations
     this.initGlobalNavY = INIT_GLOBAL_NAV_Y
     this.initHomeHeroY = INIT_HOME_HERO_Y   
     this.initHomeShelvesY = INIT_HOME_SHELVES_Y
 
     //-- set panes' up locations
-    const TOP_Y = V_MIDDLE_Y - (INIT_SHELF_Y + BASE_TITLE_H + TITLE_N_TILE_OFFSET + BASE_TILE_H/2)         //-- top of the homeShelves pane location, where the fist shelf is v-aligned with the center of the stage
-    this.paneShiftOffsetY = this.initHomeShelvesY - TOP_Y + (FOCUSED_TILE_H - BASE_TILE_H)/2               //bc, the fist shelf tile will be focused : (332-180)/2
+    this.upHomeShelvesY = V_MIDDLE_Y - (INIT_SHELF_Y + BASE_TITLE_H + TITLE_TO_TILE_OFFSET + BASE_TILE_H/2)       //-- top of the homeShelves pane location, where the fist shelf is v-aligned with the center of the stage
+    this.paneShiftOffsetY = this.initHomeShelvesY - this.upHomeShelvesY + (FOCUSED_TILE_H - BASE_TILE_H)/2        //bc, the fist shelf tile will be focused : (332-180)/2
     this.upGlobalNavY = this.initGlobalNavY - this.paneShiftOffsetY
     this.upHomeHeroY = this.initHomeHeroY - this.paneShiftOffsetY           //-- homeHeroPane shifts on homeShelves pane and its first shelf being focused
-    this.upHomeShelvesY = TOP_Y
+    //this.upHomeShelvesY = TOP_Y
 
+    //-- set panes' mid-up and off the stage locations
     this.upMidHomeHeroY = this.upHomeHeroY - BLOOMED_SHELF_SHIFT_Y          //-- homeHeroPane shifts on the fist shelf being largeBloomed
     //
     this.upOffHomeHeroY = this.upHomeHeroY - FOCUSED_SHELF_OFFSET_Y         //-- homeHeroPane shifts/hides on the 2nd shelf being focused
     this.upOffHomeShelvesY = this.upHomeShelvesY -  FOCUSED_SHELF_OFFSET_Y  //-- need?????
-    
-    this.tvEventHandler = new TVEventHandler()
-
-    // this._enableTVEventHandler = this._enableTVEventHandler.bind(this)
   }
-
-  // _registerControllable = (
-  //   onFocus,
-  //   onBlur,
-  //   onLeft,
-  //   onRight,
-  //   onUp,
-  //   onDown,
-  //   onPress,
-  // ) => {
-  //   this.state.controllables.push({
-  //     onFocus,
-  //     onBlur,
-  //     onLeft,
-  //     onRight,
-  //     onUp,
-  //     onDown,
-  //     onPress,
-  //   });
-
-  //   this.setState({ controllables: this.state.controllables });
-  // }
 
   componentDidMount() {
     this._enableTVEventHandler()
@@ -147,40 +184,34 @@ export default class POCContainer extends Component {
     console.log('INFO :: _enableTVEventHandler, this.tvEventHandler ? ' + this.tvEventHandler);
     //
     this.tvEventHandler.enable(this, function(cmp, evt) {
-      console.log('INFO :: _enableTVEventHandler, cmp? : ' + cmp.constructor.name);  //POCContainer
-      //-- sometimes works, sometimes doesn't ?????
-      //if (evt) {
+      //console.log('INFO :: _enableTVEventHandler, cmp? : ' + cmp.constructor.name);  //POCContainer
+      if (evt) {
         console.log('INFO POCContainer :: _enableTVEventHandler, evt.eventType? : ' + evt.eventType);
-        console.log('INFO POCContainer :: _enableTVEventHandler, evt.tag? : ' + evt.tag);
         switch (evt.eventType) {
           case 'blur':
-            //console.log('INFO POCContainer :: blur');
-            break;
           case 'focus':
-            //console.log('INFO POCContainer :: focus');
-            break;
-          case 'playPause':
-            //onsole.log('INFO POCContainer :: playPause');
-            break;
-          case 'rewind':
-            //console.log('INFO POCContainer :: rewind');
-            break;
-          case 'fastForward':
-            //console.log('INFO POCContainer :: fastForward');
-            break;
           case 'select':
-            //cmp._doSelect();
+            //console.log('INFO POCContainer :: _enableTVEventHandler, evt.eventType? : ' + evt.eventType);
+            console.log('INFO POCContainer :: _enableTVEventHandler, evt.tag? : ' + evt.tag);
             break;
           case 'left':
-            //cmp._doLeft();
-            break;
+          case 'right':
           case 'up':
-            //cmp._doUp();
+          case 'down':
+            console.log('INFO POCContainer :: _enableTVEventHandler, ever???? : ' + evt.eventType);
+            break;
+          case 'playPause':
+            console.log('INFO POCContainer :: playPause');
+            break;
+          case 'rewind':
+            console.log('INFO POCContainer :: rewind');
+           break;
+          case 'fastForward':
+            console.log('INFO POCContainer :: fastForward');
             break;
           default:
-            console.log('INFO POCContainer :: _enableTVEventHandler, default evt.eventType? : ' + evt.eventType);
         }//switch
-      // }//if
+      }//if
     });//enable
   }//_enableTVEventHandler
 
@@ -231,6 +262,10 @@ export default class POCContainer extends Component {
   }//_removeKeyListener
 
   _toggleGuide = () => { this.setState({isGuideVisible: !this.state.isGuideVisible}) }
+
+  _showGuide = () => { if (!this.state.isGuideVisible) this.setState({isGuideVisible: true}) }
+
+  _hideGuide = () => { if (this.state.isGuideVisible) this.setState({isGuideVisible: false}) }
 
   _doUp = () => {
     console.log("\n---")
@@ -371,17 +406,68 @@ export default class POCContainer extends Component {
   }//_changeLocation
 
   _onGlobalNavPaneFocus = () => {
-    // if (this.currFocusLocIndex !== GLOBAL_NAV_INDEX) return
-    console.log("INFO POCContainer :: _onGlobalNavPaneFocus, onFocus callback")
+    console.log("INFO POCContainer :: _onGlobalNavPaneFocus, ====================> prevFocusLocIndex ? " + this.currFocusLocIndex)
 
-    this._changeOpacity(GLOBAL_NAV_INDEX, 1)
-    this._changeOpacity(HOME_HERO_INDEX, 1)
-    this._changeOpacity(HOME_SHELVES_INDEX, 1)
+    if (this.currFocusLocIndex === GLOBAL_NAV_INDEX) return
+    
+    this._changeOpacity(GLOBAL_NAV_INDEX, SELECTED_OPACITY)
+    this._changeOpacity(HOME_HERO_INDEX, SELECTED_OPACITY)
+    this._changeOpacity(HOME_SHELVES_INDEX, SELECTED_OPACITY)
+    this._changeLocation(GLOBAL_NAV_INDEX, this.initGlobalNavY)
+    this._changeLocation(HOME_HERO_INDEX, this.initHomeHeroY)
+    this._changeLocation(HOME_SHELVES_INDEX, this.initHomeShelvesY)
+    this._hideGuide()
+
+    this.currFocusLocIndex = GLOBAL_NAV_INDEX
+    this.currHomeShelvesY = this.initHomeShelvesY
+
+    console.log("INFO POCContainer :: _onGlobalNavPaneFocus, ====================> currFocusLocIndex ? " + this.currFocusLocIndex)
   }//_onGlobalNavPaneFocus
+
+  _onHomeHeroPaneFocus = () => {
+    console.log("INFO POCContainer :: _onHomeHeroPaneFocus, =====================> prevFocusLocIndex ? " + this.currFocusLocIndex)
+
+    if (this.currFocusLocIndex === HOME_HERO_INDEX) return
+    
+    this._changeOpacity(GLOBAL_NAV_INDEX, UNSELECTED_OPACITY)
+    this._changeOpacity(HOME_HERO_INDEX, SELECTED_OPACITY)
+    this._changeOpacity(HOME_SHELVES_INDEX, UNSELECTED_OPACITY)
+    this._changeLocation(GLOBAL_NAV_INDEX, this.initGlobalNavY)
+    this._changeLocation(HOME_HERO_INDEX, this.initHomeHeroY)
+    this._changeLocation(HOME_SHELVES_INDEX, this.initHomeShelvesY)
+    this._hideGuide()
+
+    this.currFocusLocIndex = HOME_HERO_INDEX
+    this.currHomeShelvesY = this.initHomeShelvesY
+
+    console.log("INFO POCContainer :: _onHomeHeroPaneFocus, =====================> currFocusLocIndex ? " + this.currFocusLocIndex)
+  }//_onHomeHeroPaneFocus
+
+  _onShelvesPaneFocus = () => {
+    console.log("INFO POCContainer :: _onShelvesPaneFocus, ======================> prevFocusLocIndex ? " + this.currFocusLocIndex)
+
+    if (this.currFocusLocIndex === HOME_SHELVES_INDEX) return
+    
+    this._changeOpacity(GLOBAL_NAV_INDEX, UNSELECTED_OPACITY)
+    this._changeOpacity(HOME_HERO_INDEX, UNSELECTED_OPACITY)
+    this._changeOpacity(HOME_SHELVES_INDEX, SELECTED_OPACITY)
+    this._changeLocation(GLOBAL_NAV_INDEX, this.upGlobalNavY)
+    this._changeLocation(HOME_HERO_INDEX, this.upHomeHeroY)
+    this._changeLocation(HOME_SHELVES_INDEX, this.upHomeShelvesY)
+    this._showGuide()
+
+    this.currFocusLocIndex = HOME_SHELVES_INDEX
+    this.currHomeShelvesY = this.upHomeShelvesY
+
+    //TODO: CHECK
+    this.isFirstShelfSelected = true
+
+    console.log("INFO POCContainer :: _onShelvesPaneFocus, ======================> currFocusLocIndex ? " + this.currFocusLocIndex)
+  }//_onShelvesPaneFocus
 
   _onGlobalNavPaneBlur = () => {
     // if (this.currFocusLocIndex !== GLOBAL_NAV_INDEX) return
-    console.log("INFO POCContainer :: _onGlobalNavPaneBlur, onBlurCallBack")
+    console.log("INFO POCContainer :: _onGlobalNavPaneBlur, onBlurCallBack =====> POCContainer")
 
     // this._changeOpacity(GLOBAL_NAV_INDEX, 1)
     // this._changeOpacity(HOME_HERO_INDEX, 1)
@@ -390,25 +476,16 @@ export default class POCContainer extends Component {
 
   _onGlobalNavPaneSelect = () => {
     // if (this.currFocusLocIndex !== GLOBAL_NAV_INDEX) return
-    console.log("INFO POCContainer :: _onGlobalNavPaneSelect, onSelectCallBack")
+    console.log("INFO POCContainer :: _onGlobalNavPaneSelect, onSelectCallBack =====> POCContainer")
 
     // this._changeOpacity(GLOBAL_NAV_INDEX, 1)
     // this._changeOpacity(HOME_HERO_INDEX, 1)
     // this._changeOpacity(HOME_SHELVES_INDEX, 1)
   }//_onGlobalNavPaneFocus
 
-  _onHomeHeroPaneFocus = () => {
-    // if (this.currFocusLocIndex !== HOME_HERO_INDEX) return
-    console.log("INFO POCContainer :: _onHomeHeroPaneFocus, onFocusCallBack")
-
-    this._changeOpacity(GLOBAL_NAV_INDEX, .4)
-    this._changeOpacity(HOME_HERO_INDEX, 1)
-    this._changeOpacity(HOME_SHELVES_INDEX, .4)
-  }//_onHomeHeroPaneFocus
-
   _onHomeHeroPaneBlur = () => {
     // if (this.currFocusLocIndex !== HOME_HERO_INDEX) return
-    console.log("INFO POCContainer :: _onHomeHeroPaneBlur, onBlurCallBack")
+    console.log("INFO POCContainer :: _onHomeHeroPaneBlur, onBlurCallBack =====> POCContainer")
 
     // this._changeOpacity(GLOBAL_NAV_INDEX, .4)
     // this._changeOpacity(HOME_HERO_INDEX, 1)
@@ -417,24 +494,15 @@ export default class POCContainer extends Component {
 
   _onHomeHeroPaneSelect = () => {
     // if (this.currFocusLocIndex !== HOME_HERO_INDEX) return
-    console.log("INFO POCContainer :: _onHomeHeroPaneSelect, onSelectCallBack")
+    console.log("INFO POCContainer :: _onHomeHeroPaneSelect, onSelectCallBack =====> POCContainer")
 
     // this._changeOpacity(GLOBAL_NAV_INDEX, .4)
     // this._changeOpacity(HOME_HERO_INDEX, 1)
     // this._changeOpacity(HOME_SHELVES_INDEX, .4)
   }//_onHomeHeroPaneFocus
 
-  _onShelvesPaneFocus = () => {
-    //if (this.currFocusLocIndex !== HOME_SHELVES_INDEX) return
-    console.log("INFO POCContainer :: _onShelvesPaneFocus, onFocusCallBack")
-
-    this._changeOpacity(GLOBAL_NAV_INDEX, .4)
-    this._changeOpacity(HOME_HERO_INDEX, .4)
-    this._changeOpacity(HOME_SHELVES_INDEX, 1)
-  }//_onShelvesPaneFocus
-
   _onShelvesPaneBlur = () => {
-    console.log("INFO POCContainer :: _onShelvesPaneBlur, onBlurCallBack")
+    console.log("INFO POCContainer :: _onShelvesPaneBlur, onBlurCallBack =====> POCContainer")
 
     // this.currFocusLocIndex = HOME_HERO_INDEX
 
@@ -451,7 +519,7 @@ export default class POCContainer extends Component {
 
   _onShelvesPaneSelect = () => {
     // if (this.currFocusLocIndex !== HOME_HERO_INDEX) return
-    console.log("INFO POCContainer :: _onShelvesPaneSelect, onSelectCallBack")
+    console.log("INFO POCContainer :: _onShelvesPaneSelect, onSelectCallBack =====> POCContainer")
 
     // this._changeOpacity(GLOBAL_NAV_INDEX, .4)
     // this._changeOpacity(HOME_HERO_INDEX, 1)
@@ -520,7 +588,7 @@ export default class POCContainer extends Component {
   }//_renderGuide
 
   render() {
-    //console.log("INFO POCContainer :: render")
+    console.log("INFO POCContainer :: render")
     let { animOpacity0, animOpacity1, animOpacity2, animLocation0, animLocation1, animLocation2} = this.state
     return (
         <View style={styles.pocContainer}>
--- src/config.js
+++ src/config.js
@@ -2,13 +2,13 @@ import {PixelRatio} from 'react-native';
 
 export default {
   //-- base dimension and density
-  density: PixelRatio.get(),    //TV density
+  density: PixelRatio.get(),    //-- TV density
   stageW: 1920,
   stageH: 1080,
 
   //-- animation related
-  stdDuration: 500,             //in milliseconds = .5 sec,
-  shortDuration: 350,           //in milliseconds = .35 sec,
+  stdDuration: 500,             //-- in milliseconds = .5 sec,
+  shortDuration: 350,           //-- in milliseconds = .35 sec,
   waitToLargeBloomDuration: 4000,
 
   //-- pane initial location
@@ -20,8 +20,43 @@ export default {
   initGlobalNavH: 100,
   initHomeHeroH: 606,
 
+  //-- opacity related
+  unselectedOpacity: .5,        //-- .6 -> .5, for testing
+  selectedOpacity: 1,
+
   //-- homeShelvesPane location & dimension related
   homeShelves: {
+    initShelfY: 62,             //-- distance from the top of homeShelvesPane container to the top of 1st shelf container
+
+    //-- shelf title related
+    baseTitleH: 40,             //-- unselected shelf title height (!!!not same as the font size, 28)
+    focusedTitleH: 60,          //-- update!!!
+    titleToTileOffset: 10,      //-- distance from the bottom of shelf title to the top of shelf tile
     
+    //-- tile dimensions
+    baseTileW: 320,             //-- unselected base tile width, 320x180
+    baseTileH: 180,             //-- unselected base tile height
+    focusedTileW: 590,          //-- focused tile width, on a selected shelf, 590x332
+    focusedTileH: 332,          //-- focused tile height, on a selected shelf
+    bloomedTileW: 1056,         //-- large bloomed tile width, on a selected shelf, 1056x594
+    bloomedTileH: 594,          //-- large bloomed tile height, on a selected shelf
+    // ??? focused unselected tile dimension ??? (375x210)
+    // ??? bloomed unselected tile dimension ??? (782x440)
+
+    //-- shelf related
+    baseShelfOffsetX: 0,        //-- distance between tiles on an unselected shelf
+    baseShelfOffsetY: 106,      //-- distance between unselected shelves
+    baseShelfH: 336,            //-- baseTitleH (40) + titleToTileOffset (10) + baseTileH (180) + baseShelfOffsetY (106) = 336
+
+    focusedShelfShiftY: 76,     //-- the y location shift of unselected shelves on selected shelf being focused: (focusedTileH (332) - baseTileH (180))/2 = 76
+    bloomedShelfShiftY: 131,    //-- the y location shift of unselected shelves on selected shelf being large bloomed: (bloomedTileH (594) - focusedTileH (332))/2 = 131
+    
+    focusedShelfOffsetX: 25,    //-- distance between tiles on a focused shelf
+    focusedShelfOffsetY: 182,   //-- baseShelfOffsetY (106) + focusedShelfShiftY (76) = 182
+    focusedShelfH: 584,         //-- focusedTitleH (60) + titleToTileOffset (10) + focusedTileH (332) + focusedShelfOffsetY (182) = 584
+
+    bloomedShelfOffsetX: 57,    //-- distance between tiles on a focused shelf ?????
+    bloomedShelfOffsetY: 313,   //-- focusedShelfOffsetY (182) + bloomedShelfShiftY (131) = 313
+    bloomedShelfH: 543          //-- focusedTitleH (60) + titleToTileOffset (10) + bloomedTileH (594) + bloomedShelfOffsetY (313) = 977
   }
 };
